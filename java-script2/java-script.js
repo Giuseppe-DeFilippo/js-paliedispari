d1a6f88bdfb455a221a569f800b1a1438dc2e460
giuse <giuseppedefilippo03@gmail.com>
2024-11-05 15:32:15 +0100

    add functions

--- java-script2/java-script.js
+++ java-script2/java-script.js
@@ -1,6 +1,28 @@
-console.clear();
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
 
-const pd = prompt("pari o dispari?");
-console.log(pd);
-const number = parseInt(prompt("inserisci numero da 1 a 5"));
-console.log(number);
+
+console.clear();
+    const pd = prompt("pari o dispari?");
+    const number = parseInt(prompt("inserisci numero da 1 a 5"));
+    console.log(pd);
+    console.log(number);
+
+function random(){
+const computer = Math.floor(Math.random() * 6);
+console.log(computer);
+}
+
+//function paridispari(){
+    
+//}
+//questa funzione serve per somamre i 2 numeri e vedere se sono pari e dispari 
+function winner(){
+
+}
+
+//paridispari();
+random(); 
