0da108f94f9cc41cb1352a6b61116cddca76142c
giuse <giuseppedefilippo03@gmail.com>
2024-11-05 14:56:35 +0100

    numero 1

---
+++ java-script1/java.js
@@ -0,0 +1,25 @@
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+Pari e Dispari*/
+
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+
+console.clear()
+let word = prompt("inserisci la parola");
+let wordreverse = word.split("").reverse().join("");
+function parola(word, wordreverse){
+    if (word === wordreverse){
+        console.log("è una parola palindroma");
+    }
+    else{
+        console.log("non è una parola palindroma");
+    }
+}
+parola(word, wordreverse);
+// perchè nell if non andava il  .reverse
